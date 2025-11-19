07ce1f56922489b5d378f146b99fa4d7abbc7864
princebyun <princebyun@gmail.com>
2025-11-20 00:48:24 +0900

    feat : 자동차 이름 검증 로직 추가

--- lib/logic/racing_logic.dart
+++ lib/logic/racing_logic.dart
@@ -1,4 +1,10 @@
 class Car {
-  String name = "";
+  final String name;
   int position = 0;
+
+  Car(this.name) {
+    if (this.name.isEmpty || this.name.length > 5) {
+      throw Exception("이름은 1자 이상 5자 이하여야 합니다.");
+    }
+  }
 }
