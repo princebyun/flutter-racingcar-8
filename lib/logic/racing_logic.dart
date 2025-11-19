e8cc46a468bece38c89174f94cf069551a9881ce
princebyun <princebyun@gmail.com>
2025-11-20 01:37:34 +0900

    feat : GameRound 클래스 및 참가자 등록

--- lib/logic/racing_logic.dart
+++ lib/logic/racing_logic.dart
@@ -1,5 +1,14 @@
 import 'dart:math';
 
+void main() {
+  GameRound round = GameRound();
+  round.setCars("pobi,crong,honux");
+
+  // 확인용 출력
+  print("참가자 수: ${round.carList.length}");
+  print("첫 번째 차: ${round.carList[0].name}");
+}
+
 class Car {
   final String name;
   int position = 0;
@@ -14,6 +23,17 @@ class Car {
     int value = Random().nextInt(10);
     if (value >= 4) {
       this.position++;
+    }
+  }
+}
+
+class GameRound {
+  List<Car> carList = [];
+
+  setCars(String carNames) {
+    List<String> names = carNames.split(",");
+    for (String name in names) {
+      carList.add(new Car(name));
     }
   }
 }
