146203b4e305eb2d88a1bd21cafc18ac95d4ecf2
princebyun <princebyun@gmail.com>
2025-11-20 01:24:52 +0900

    feat : 자동차 이동 로직 구현

--- lib/logic/racing_logic.dart
+++ lib/logic/racing_logic.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Car {
   final String name;
   int position = 0;
@@ -5,6 +7,13 @@ class Car {
   Car(this.name) {
     if (this.name.isEmpty || this.name.length > 5) {
       throw Exception("이름은 1자 이상 5자 이하여야 합니다.");
+    }
+  }
+
+  void move() {
+    int value = Random().nextInt(10);
+    if (value >= 4) {
+      this.position++;
     }
   }
 }
